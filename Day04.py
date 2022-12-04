7466c171270e8d53b83e14052f2e974a53539a5c
Ofresca <tamaraspanier@gmail.com>
2022-12-04 13:23:21 +0100

    Initial commit

---
+++ Day04.py
@@ -0,0 +1,23 @@
+"""
+Day 04 of Advent of Code
+"""
+
+import re
+
+input = open("Day04-input.txt",'r').read().split('\n')
+
+counter = 0
+for line in input:
+    min1, max1, min2, max2 = map(int, re.split(',|-',line))
+    if (min1 <= min2 and max1 >= max2) or (min1 >= min2 and max1 <= max2):
+        counter += 1
+    
+print('Part 1:', counter)
+
+counter = 0
+for line in input:
+    min1, max1, min2, max2 = map(int, re.split(',|-',line))
+    if (max1 >= min2 and max1 <= max2) or (max2 >= min1 and max1 >= max2):
+        counter += 1
+
+print('Part 2:', counter)
