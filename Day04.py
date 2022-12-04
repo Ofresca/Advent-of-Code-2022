3e66bf23a5aa326e4a68761516c7774c98ddf9f1
Ofresca <tamaraspanier@gmail.com>
2022-12-04 14:14:33 +0100

    not

--- Day04.py
+++ Day04.py
@@ -17,7 +17,7 @@ print('Part 1:', counter)
 counter = 0
 for line in input:
     min1, max1, min2, max2 = map(int, re.split(',|-',line))
-    if (max1 >= min2 and max1 <= max2) or (max2 >= min1 and max1 >= max2):
+    if not (min1 > max2 or min2 > max1):
         counter += 1
 
 print('Part 2:', counter)
