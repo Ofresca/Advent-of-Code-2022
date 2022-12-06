e0538c084a1048870a06ec95d77e663f699f9d57
Ofresca <tamaraspanier@gmail.com>
2022-12-06 17:55:08 +0100

    Cleanup

--- Day06.py
+++ Day06.py
@@ -5,21 +5,15 @@ Day 06 of Advent of Code
 input = open("Day06-input.txt",'r').read()
 
 len_str = 4
-
 for i in range(len_str,len(input)):
-    test_string = input[(i-len_str):i]
-    if len(set(test_string)) == len_str:
-        result = i
+    if len(set(input[(i-len_str):i])) == len_str:
         break
 
-print('Part 1:', result)
+print('Part 1:', i)
 
 len_str = 14
-
 for i in range(len_str,len(input)):
-    test_string = input[(i-len_str):i]
-    if len(set(test_string)) == len_str:
-        result = i
+    if len(set(input[(i-len_str):i])) == len_str:
         break
 
-print('Part 2:', result)
+print('Part 2:', i)
